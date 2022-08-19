f194a54315926c23b9f2409933c2b074f7c8365d
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-19 22:44:44 +0530

    Updates

---
+++ precache-manifest.18bb5a5eefdefba68446beb168477a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07c3db0e447198aafbea",
+    "url": "/TextUtils/static/css/main.f03d4be8.chunk.css"
+  },
+  {
+    "revision": "07c3db0e447198aafbea",
+    "url": "/TextUtils/static/js/main.07c3db0e.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "13dc048470cafba891fd",
+    "url": "/TextUtils/static/js/2.13dc0484.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "b4237b65777224f5b1e575a9d509d5e9",
+    "url": "/TextUtils/index.html"
+  }
+];
